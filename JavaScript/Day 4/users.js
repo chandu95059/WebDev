6bc301a36245844cb1167d3be870102538f775f1
chandu95059 <chandu95059@gmail.com>
2023-11-21 23:39:21 +0530

    updating daily learnings

---
+++ JavaScript/Day 4/users.js
@@ -0,0 +1,51 @@
+let users=[
+    {
+      "firstname": "Ewing",
+      "lastname": " Goodman",
+      "gender": "male",
+      "email": "ewinggoodman@bizmatic.com",
+      "address": "365 Kossuth Place, Drummond, Montana, 8673"
+    },
+    {
+      "firstname": "Shannon",
+      "lastname": " King",
+      "gender": "female",
+      "email": "shannonking@bizmatic.com",
+      "address": "248 Nevins Street, Blende, Nebraska, 7628"
+    },
+    {
+      "firstname": "Estrada",
+      "lastname": " Griffin",
+      "gender": "male",
+      "email": "estradagriffin@bizmatic.com",
+      "address": "825 Lake Street, Woodlands, Illinois, 514"
+    },
+    {
+      "firstname": "Huff",
+      "lastname": " Mclaughlin",
+      "gender": "male",
+      "email": "huffmclaughlin@bizmatic.com",
+      "address": "259 Livonia Avenue, Watchtower, American Samoa, 787"
+    },
+    {
+      "firstname": "Gray",
+      "lastname": " Russo",
+      "gender": "male",
+      "email": "grayrusso@bizmatic.com",
+      "address": "286 Vanderveer Place, Unionville, Utah, 729"
+    },
+    {
+      "firstname": "Lindsey",
+      "lastname": " Pratt",
+      "gender": "female",
+      "email": "lindseypratt@bizmatic.com",
+      "address": "636 Sedgwick Place, Abrams, Missouri, 4969"
+    },
+    {
+      "firstname": "Oconnor",
+      "lastname": " Gibson",
+      "gender": "male",
+      "email": "oconnorgibson@bizmatic.com",
+      "address": "204 Seigel Court, Ilchester, Michigan, 8497"
+    }
+  ]
